438e99eab5586736f03d847211174d0e05b18679
Art Stoupin <art@stoupin.ru>
2020-03-30 02:27:02 +0300

    ok

--- src/test/java/ru/cetelem/nexushook/NexusHookApplicationTests.java
+++ src/test/java/ru/cetelem/nexushook/NexusHookApplicationTests.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
-import ru.cetelem.nexushook.service.JenkinsService;
 import ru.cetelem.nexushook.service.NexusHookService;
 import ru.cetelem.nexushook.web.NexusHookController;
 
